ffdba0508a000fa80742b5ffaf362c16b34e7fba
elcho <elcho@qq.com>
2019-12-14 12:48:36 +0800

    provider company

---
+++ provider-company-api/src/main/java/com/elcho/providercompanyapi/ProviderCompanyApiApplication.java
@@ -0,0 +1,13 @@
+package com.elcho.providercompanyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderCompanyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderCompanyApiApplication.class, args);
+    }
+
+}
---
+++ provider-company-api/src/test/java/com/elcho/providercompanyapi/ProviderCompanyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcho.providercompanyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderCompanyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
