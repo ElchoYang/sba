e11800db98c1e42b86853b3b997fba130f9d58bd
elcho <elcho@qq.com>
2019-12-14 12:49:32 +0800

    provider ipo

---
+++ provider-ipo-api/src/main/java/com/elcho/provideripoapi/ProviderIpoApiApplication.java
@@ -0,0 +1,13 @@
+package com.elcho.provideripoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderIpoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderIpoApiApplication.class, args);
+    }
+
+}
---
+++ provider-ipo-api/src/test/java/com/elcho/provideripoapi/ProviderIpoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcho.provideripoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderIpoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
