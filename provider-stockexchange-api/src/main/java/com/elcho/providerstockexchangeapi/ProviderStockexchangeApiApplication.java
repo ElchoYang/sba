64aa4db26af698c7a3dc8b528a623a612f6ba69e
elcho <elcho@qq.com>
2019-12-14 12:50:20 +0800

    provider stockexchange

---
+++ provider-stockexchange-api/src/main/java/com/elcho/providerstockexchangeapi/ProviderStockexchangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.elcho.providerstockexchangeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderStockexchangeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderStockexchangeApiApplication.class, args);
+    }
+
+}
---
+++ provider-stockexchange-api/src/test/java/com/elcho/providerstockexchangeapi/ProviderStockexchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcho.providerstockexchangeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderStockexchangeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
