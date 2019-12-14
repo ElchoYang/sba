48d9f284ae519c42a8293272ae74bfef84dae8a3
elcho <elcho@qq.com>
2019-12-14 12:37:24 +0800

    ureka server

---
+++ eureka-server/src/main/java/com/elcho/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.elcho.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/***
+ * Regist
+ * 1.  config Eureka
+ * 2. Enable
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
